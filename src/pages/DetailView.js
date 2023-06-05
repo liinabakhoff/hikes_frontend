3ac906bba1b322f3fcbb1966e29bab11c966c1f6
Liina-Maria Bakhoff <96503564+liinabakhoff@users.noreply.github.com>
2023-06-05 15:52:58 +0300

    Show DetailView data based on url params

--- src/pages/DetailView.js
+++ src/pages/DetailView.js
@@ -2,12 +2,17 @@ import { useParams } from 'react-router-dom'
 
 function DetailView({ hikes }) {
 	const { hike_id } = useParams()
+	const hike = hikes.find((item) => item.id === parseInt(hike_id, 10))
+	console.log('0hike_id', hike_id)
+	console.log('1hikes', hikes)
+	console.log('2hike', hike)
 	if (hikes?.length < 1) {
 		return <div>Laen postitusi</div>
 	}
 	return (
 		<div>
 			Näitan matka id-ga <b>{hike_id}</b>
+			<h1>{hike?.name}</h1>
 		</div>
 	)
 }
